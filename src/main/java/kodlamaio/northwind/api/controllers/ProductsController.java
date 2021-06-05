9a7c3be30f4f1cc207fec843f0b6722452470a52
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-06-06 01:58:15 +0400

    CORS

--- src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -1,6 +1,7 @@
 package kodlamaio.northwind.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @RestController // sen controllersun demekdir
 @RequestMapping("/api/products") // coxlu controller temzinleme
+@CrossOrigin
 public class ProductsController {
 
 	private ProductService productService;
